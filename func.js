0f895c12aec92771acafd2fbbdd61b4dfca68d09
AstronautaSze <sabalozua@outlook.pt>
2023-12-29 21:05:53 +0100

    Subindo o protifolio para o servidor

--- func.js
+++ func.js
@@ -4,17 +4,30 @@ const nome=document.getElementById('nome')
 const btn_enviar=document.getElementById('btn_enviar')
 const ideia=document.getElementById('ideia');
 
-btn_enviar.addEventListener('click', (envet)=>{
-  const servidor='https://meu-portifolio-vert-five.vercel.app';
-  fetch(`${servidor}/send/${email.value}/${ideia.value}/${nome.value}`)
+btn_enviar.addEventListener('click',async (envet)=>{
+  envet.preventDefault();
+  
+  const Dados={
+  email_clinent:email.value,
+  nome:nome.value,
+  ideia:ideia.value
+  }
+  const cabecalho = {
+    method: 'post',
+    body: Dados.email_clinent
+  }
+
+  const servidor='https://api-email-five.vercel.app';
+ await fetch(`${servidor}/send`,cabecalho)
+  
   .then(res=>{
     if(res.status==200){
       alert(res.status)
-      console.log('resoltou')
+      console.log(Dados)
     }else{
         alert(res.status)
         console.log('nao')
     }
-  })
-    
+})
+
  })
