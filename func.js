e040c0f89efae59eece11e7d9b35533d8fc5e6d0
AstronautaSze <sabalozua@outlook.pt>
2024-02-14 20:07:47 +0100

    intruduzindo novas funcionalidades

--- func.js
+++ func.js
@@ -3,14 +3,24 @@ const email=document.getElementById('email')
 const nome=document.getElementById('nome')
 const btn_enviar=document.getElementById('btn_enviar')
 const ideia=document.getElementById('ideia');
+let LGButton=document.getElementById('LGButton')
+const spinner=document.getElementById('animacao')
 const resetar=()=>{
   email.value="";
   nome.value="";
   ideia.value="";
+  LGButton.innerText="Enviado"
+  spinner.classList.add("ocultar")
 }
 btn_enviar.addEventListener('click',async (envet)=>{
   envet.preventDefault();
-  
+  if(nome.value=="" || ideia.value=="" || email.value=="")
+  return;
+
+  else{
+  LGButton.innerText="A enviar..."
+  spinner.classList.remove("ocultar")
+
  const  Dados={
   email_clinent:email.value,
   nome:nome.value,
@@ -22,16 +32,17 @@ btn_enviar.addEventListener('click',async (envet)=>{
     headers: {"Content-type": "application/json; charset=UTF-8"}
   }
 
+
   const servidor='https://api-email-six.vercel.app';
   fetch(`${servidor}/send`,cabecalho)
   
+
   .then(res=>{
     if(res.status==200){
-      alert("Email envia do com sucesso") 
       resetar();
     }else{
       alert("Erro")
     }
 })
-
+}
  })
